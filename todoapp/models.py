95051af62490015c7ad1c0299f42ffddfd3c3b3d
Steve Geluso <stevegeluso@gmail.com>
2017-02-08 14:42:38 -0800

    adding User relation to todo, creating and running migrations

--- todoapp/models.py
+++ todoapp/models.py
@@ -1,7 +1,10 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Todo(models.Model):
     text = models.CharField(max_length=150)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    
     def __str__(self):
         return self.text
