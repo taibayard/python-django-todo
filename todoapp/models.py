2e915efab6ddbe9fb55799f1ad4e7f2b96cc4316
Steve Geluso <stevegeluso@gmail.com>
2017-02-08 15:40:19 -0800

    adding is_complete boolean field

--- todoapp/models.py
+++ todoapp/models.py
@@ -5,6 +5,7 @@ from django.contrib.auth.models import User
 class Todo(models.Model):
     text = models.CharField(max_length=150)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
+    is_complete = models.BooleanField(default=False)
     
     def __str__(self):
         return self.text
