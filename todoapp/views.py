0e8392fcba39e054d416f33f04cfd91cc93f8290
srobertson421 <srobertson421@gmail.com>
2017-02-08 12:39:53 -0800

    Added form for adding todos, check for POST request

--- todoapp/views.py
+++ todoapp/views.py
@@ -1,9 +1,15 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import Todo
 
 # Create your views here.
 def index(request):
-    context = {
-        "todos": Todo.objects.all()
-    }
-    return render(request, 'todoapp/index.html', context)
+    if request.method == "GET":
+        context = {
+            "todos": Todo.objects.all()
+        }
+        return render(request, 'todoapp/index.html', context)
+    elif request.method == "POST":
+        new_todo = Todo()
+        new_todo.text = request.POST["text"]
+        new_todo.save()
+        return redirect('index')
