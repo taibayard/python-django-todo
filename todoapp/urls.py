0283e8e47fad91b0bd56009072bc002fdf875eb9
srobertson421 <srobertson421@gmail.com>
2017-02-08 16:57:18 -0800

    Auth setup

--- todoapp/urls.py
+++ todoapp/urls.py
@@ -2,5 +2,9 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name="index")
+    url(r'^$', views.index, name="index"),
+    url(r'^signup$', views.signup, name="signup"),
+    url(r'^login$', views.login, name="login"),
+    url(r'^logout$', views.logout, name="logout"),
+    url(r'^secret$', views.secret, name="secret"),
 ]
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
 from .models import Todo
 
 # Create your views here.
@@ -13,3 +15,41 @@ def index(request):
         new_todo.text = request.POST["text"]
         new_todo.save()
         return redirect('index')
+
+def signup(request):
+    context = {"error": False}
+    if request.method == "GET":
+        return render(request, 'todoapp/signup.html', context)
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        try:
+            user = User.objects.create_user(username=username, password=password)
+            if user is not None:
+                return login(request)
+        except:
+            context["error"] = f"User {username} already exists"
+            return render(request, 'todoapp/signup', context)
+
+def login(request):
+    if request.method == "GET":
+        return render(request, 'todoapp/login.html')
+    elif request.method == "POST":
+        username = request.POST["username"]
+        password = request.POST["password"]
+        user = auth.authenticate(username=username, password=password)
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+
+def logout(request):
+    auth.logout(request)
+    return redirect('/')
+
+def secret(request):
+    context = {"error": False}
+    if request.user.is_authenticated:
+        return render(request, 'todoapp/secret.html')
+    else:
+        context["error"] = "Not authenticated"
+        return render(request, 'todoapp/signup.html', context)
