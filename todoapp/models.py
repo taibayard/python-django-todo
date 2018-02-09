1e04a98c02703fb8d3d9447507d04667acb7a392
brandiw <brandimichelle.0911@gmail.com>
2018-02-09 09:04:13 -0800

    Start code

--- todoapp/models.py
+++ todoapp/models.py
@@ -2,10 +2,3 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-class Todo(models.Model):
-    text = models.CharField(max_length=150)
-    user = models.ForeignKey(User, on_delete=models.CASCADE, blank=True, null=True)
-    is_complete = models.BooleanField(default=False)
-    
-    def __str__(self):
-        return self.text
--- todoapp/views.py
+++ todoapp/views.py
@@ -1,51 +1,21 @@
 from django.contrib.auth.models import User
 from django.contrib import auth
 from django.shortcuts import render, redirect
+from django.http import HttpResponse
 from .models import Todo
 
 # Create your views here.
 def index(request):
     if request.method == "GET":
-        context = {
-            "todos": Todo.objects.all(),
-            "users": User.objects.all()
-        }
-        return render(request, 'todoapp/index.html', context)
+        return HttpResponse('index GET')
     elif request.method == "POST":
-        userid = request.POST["userid"]
-        
-        new_todo = Todo()
-        new_todo.text = request.POST["text"]
-        new_todo.save()
-        return redirect('index')
-    
+        return HttpResponse('index POST')
+
 def signup(request):
-    context = {"error": False}
-    if request.method == "GET":
-        return render(request, 'todoapp/signup.html', context)
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        try:
-            user = User.objects.create_user(username=username, password=password)
-            if user is not None:
-                return login(request)
-        except:
-            context["error"] = f"Username '{username}' already exists."
-            return render(request, 'todoapp/signup.html', context)
-    
+    return HttpResponse('signup')
+
 def login(request):
-    context = {"error": False}
-    if request.method == "GET":
-        return render(request, 'todoapp/login.html')
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        user = auth.authenticate(username=username, password=password)
-        if user is not None:
-            auth.login(request, user)
-            return redirect('index')
-            
+    return HttpResponse('login')
+
 def logout(request):
-    auth.logout(request)
-    return redirect('index')
+    return HttpResponse('logout')
