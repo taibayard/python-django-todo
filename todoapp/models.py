f426ca91c3cbe4c4d9de0a3f76678025549336e3
Admin <admin@Admins-MacBook-Pro-3.local>
2018-02-09 15:12:40 -0800

    able to add, remove, and check 'done'. Added api call

--- todoapp/models.py
+++ todoapp/models.py
@@ -3,7 +3,7 @@ from django.contrib.auth.models import User
 
 class Todo(models.Model):
     text = models.CharField(max_length=250)
-    is_complete = models.BooleanField(default=True)
+    is_complete = models.BooleanField(default=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, blank=False, null=False)
 
     def __str__(self):
--- todoapp/views.py
+++ todoapp/views.py
@@ -3,21 +3,41 @@ from django.contrib import auth
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
+import requests
+import json
 
 # Main routes
 def index(request):
     if request.method == "GET":
         todos = Todo.objects.all().order_by("text")
         users = User.objects.all()
-        return render( request, 'todoapp/index.html', {"todos":todos,"users":users})
+        r = requests.get("http://quotesondesign.com/wp-json/posts?filter[orderby]=rand&filter[posts_per_page]=1&callback=")
+        quote = json.loads(r.text)
+        return render( request, 'todoapp/index.html', {"todos":todos,"users":users,"quote":quote[0]})
     elif request.method == "POST":
-        return HttpResponse('index POST')
+        try:
+            userid=request.POST["userid"]
+        except (KeyValueError):
+            return redner(request, "todoapp/index.html", {"error":"You must select a owner"})
+        else:
+            new_todo =  Todo()
+            new_todo.text = request.POST["text"]
+            new_todo.user = User.objects.get(pk=userid)
+            new_todo.save()
+            return redirect("index")
 
 def delete(request, todo_id):
-    return HttpResponse("Delete this")
+    item = Todo.objects.get(id=todo_id)
+    item.delete()
+    return redirect("index")
+    #Alternate syntax for delete
+    #Todo.objects.filter(id=todo_id).delete()
 
 def done(request, todo_id):
-    return HttpResponse("Mark done")
+    item = Todo.objects.get(id=todo_id)
+    item.is_complete = True
+    item.save()
+    return redirect("index")
 
 # Auth-related routes
 def signup(request):
