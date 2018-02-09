867529d96ef0c62d8bf6f07e2c15e4271ad696b1
brandiw <brandimichelle.0911@gmail.com>
2018-02-09 09:13:15 -0800

    Adding delete and done routes

--- todoapp/views.py
+++ todoapp/views.py
@@ -4,13 +4,20 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import Todo
 
-# Create your views here.
+# Main routes
 def index(request):
     if request.method == "GET":
         return HttpResponse('index GET')
     elif request.method == "POST":
         return HttpResponse('index POST')
 
+def delete(request, todo_id):
+    return HttpResponse("Delete this")
+
+def delete(request, todo_id):
+    return HttpResponse("Mark done")
+
+# Auth-related routes
 def signup(request):
     return HttpResponse('signup')
 
